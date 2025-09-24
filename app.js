6ed8c0ba326f26766d60ab3b128bea19cc21600f
Adnan Rao <adnanejaz2874@gmail.com>
2025-09-24 18:36:02 +0500

    1st lecture

---
+++ app.js
@@ -0,0 +1,56 @@
+// alert("Welcom To Mine Web")
+// console.log("Rao Shahbaz")
+
+// document.write("Shahbaz")
+
+
+
+// Veriables
+
+// Name Dana k liy
+// Data ko store krny liy
+// Changeing
+
+
+// var className  = "JS CLASS"
+// var className2  = "React CLASS"
+
+// var myAge =  21
+
+
+// var totalCalss = className + className2
+
+
+// Concatination
+
+
+
+// console.log(totalCalss)
+
+
+
+
+// var className = "JS Class"
+
+
+// className = "REACT CLASS"
+
+
+// const className = "JS Class"
+
+// className = "React Class"
+
+// console.log(className)
+
+// let x = Number(prompt("Typer Your  Number1 "))
+// let y = +prompt("Typer Your  Number2 ")
+
+// let newX = x * y
+// let addtion = x + y
+
+// console.log(newX)
+// console.log(addtion)
+
+
+
+
